ac913922fb8e30b241f4abbb860c7ad2e37956eb
raizhabsyah24 <habsyahrais24@gmail.com>
2021-10-05 11:58:03 +0700

    commit -m ("first commit")

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './Pages/Login';
+import Home from './Pages/Home';
+import Header from './Component/Header';
+import Register from './Pages/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    if (localStorage.getItem("token") != null) {
+      return (
+        <Router>
+          <div
+            style={{
+              backgroundColor: "#F5F5DC",
+              minHeight: "100vh"
+            }}
+          >
+            <Header />
+            <Switch>
+            <Route path="/register">
+                <Register/>
+              </Route>
+              <Route path="/login">
+                <Login/>
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+    } else {
+      return (
+        <Router>
+          <div
+            style={{
+              backgroundColor: "#F5F5DC",
+              minHeight: "100vh"
+            }}
+          >
+            <Header />
+            <Switch>
+            <Route path="/register">
+                <Register/>
+              </Route>
+              <Route path="/login">
+                <Login/>
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
+  }
 }
-
-export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '../App.css';
+import I001 from "../Image/001.png";
+import I002 from "../Image/002.png";
+import I003 from "../Image/003.png";
+import I004 from "../Image/004.png";
+import I005 from "../Image/005.png";
+import Footer from "../Component/Footer"
+
+class Home extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      autoplay: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+      <div className="container">
+        <Slider {...settings}>
+        <div><img src={I001} alt="Credit to Joshua Earle on Unsplash"/></div>
+        <div><img src={I002} alt="Credit to Alisa Anton on Unsplash"/></div>
+        <div><img src={I003} alt="Credit to Igor Ovsyannykov on Unsplash"/></div>
+        <div><img src={I004} alt="Credit to Pierre Châtel-Innocenti on Unsplash"/></div>
+        <div><img src={I005} alt="Credit to Richard Nolan on Unsplash"/></div>
+        </Slider>
+      </div>
+        {/* <div style={{paddingTop:100}}>
+        <Footer />
+        </div> */}
+        </div>
+    );
+  }
+}
+
+export default Home;
