49a666c8c55f61e14de9b9183582b646929b017d
raizhabsyah24 <habsyahrais24@gmail.com>
2021-10-07 08:02:41 +0700

    commit -m (7 okt 2021)

---
+++ BE/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'databasebh'
+});
+
+app.get("/", (req, res) => {
+});
+
+app.listen(3001, () => {
+    console.log("Running on port 3001");
+});
---
+++ FE/src/Component/Header.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button
+} from 'react-bootstrap';
+import { Container } from "@material-ui/core";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar style={{ backgroundColor: "#6495ED" }} expand="lg">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Navbar.Brand href="/">Bimbel Hexa</Navbar.Brand>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Promo</Nav.Link>
+              <Nav.Link href="#action3">Beasiswa</Nav.Link>
+              <Nav.Link href="#action4">Kompetisi Saint</Nav.Link>
+              <NavDropdown title="Hexa Desk" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action5">Hexa Les</NavDropdown.Item>
+                <NavDropdown.Item href="#action6">Hexa Kursus</NavDropdown.Item>
+                <NavDropdown.Item href="#action7">Hexa Pelatihan</NavDropdown.Item>
+                <NavDropdown.Item href="#action8">Hexa Programing</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action9">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Button href="/login" variant="outline-success">Login</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
